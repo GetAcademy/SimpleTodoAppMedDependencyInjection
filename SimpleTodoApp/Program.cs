6657fad70e5e4a47a2b0b0fa3c2b5e3339ec9bfb
GET Academy <terje@kolderup.net>
2022-11-08 15:53:12 +0100

    Update Program.cs

--- SimpleTodoApp/Program.cs
+++ SimpleTodoApp/Program.cs
@@ -1,27 +1,35 @@
 using System.Data.SqlClient;
 using Dapper;
+using SimpleTodoApp.DomainServices;
+using SimpleTodoApp.Infrastructure;
 using SimpleTodoApp.Model;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Services.AddScoped<ITodoItemRepository, TodoItemRepository>();
+
 const string connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Todo;Integrated Security=True";
+var sqlConnectionFactory = new SqlConnectionFactory(connStr);
+builder.Services.AddSingleton(sqlConnectionFactory);
 
-app.MapGet("/todo", async () =>
+var app = builder.Build();
+
+
+app.MapGet("/todo", async (ITodoItemRepository repo) =>
 {
-
+    return repo.GetAll();
 });
-app.MapPost("/todo", async (TodoItem todoItem) =>
+app.MapPost("/todo", async (TodoItem todoItem, ITodoItemRepository repo) =>
 {
-
+    return await repo.Create(todoItem);
 });
-app.MapPut("/todo", async (TodoItem todoItem) =>
+app.MapPut("/todo", async (TodoItem todoItem, ITodoItemRepository repo) =>
 {
     todoItem.Done = DateTime.Today;
-
+    return await repo.Update(todoItem);
 });
-app.MapDelete("/todo/{id}", async (Guid id) =>
+app.MapDelete("/todo/{id}", async (Guid id, ITodoItemRepository repo) =>
 {
-
+    await repo.Delete(id);
 });
 
 app.UseStaticFiles();
